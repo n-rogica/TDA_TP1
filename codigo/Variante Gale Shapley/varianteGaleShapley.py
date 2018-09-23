0ba74fcd50273e9272a1a73f16761e70e86c1ea2
leandevesa-despegar <leandevesa@gmail.com>
2018-09-23 16:49:16 -0300

    init data structures

--- codigo/Variante Gale Shapley/varianteGaleShapley.py
+++ codigo/Variante Gale Shapley/varianteGaleShapley.py
@@ -1,4 +1,5 @@
 from sys import argv
+from collections import defaultdict
 import random
 import os
 
@@ -25,31 +26,70 @@ def genArchivosRandom(cantRecitales, cantBandas):
 
 def getDic(cant, nombre):
 
-    print("get dic ok")
+    rank = defaultdict(dict)
+    
+    for num in range(1, cant + 1):
+        snum = str(num)
+        nombreArchivo = DATA_PATH + nombre + "_" + snum + ".dat"
+        with open(nombreArchivo) as f:
+            preferencias = f.read().splitlines()
+        for i, preferencia in enumerate(preferencias):
+            rank[snum][i] = preferencia
 
-def match(cantRecitales, cantBandas, 
+    print("get " + nombre + " dic ok")
+    return rank
+
+def init(generarArchivos, cantRecitales, cantBandas, 
+        maxBandasDistintasAContratar, maxRecitalesAParticiparPorBanda):
+
+    if generarArchivos:
+        genArchivosRandom(cantRecitales, cantBandas)
+
+    ranking = defaultdict(dict)
+    ranking[BANDA] = getDic(cantBandas, BANDA)
+    ranking[RECITAL] = getDic(cantRecitales, RECITAL)
+
+    print("init ok")
+
+    return ranking
+
+def match(bandasRanking, recitalesRanking, 
         maxBandasDistintasAContratar, maxRecitalesAParticiparPorBanda):
 
     print("match ok")
 
 def main():
 
-    argv = [True,10,10,10,10] # TODO: Delete this test line
+    argv = [False,10,10,10,10] # TODO: Delete this test line
 
     if(len(argv) < 5):
         print ("cantidad de parametros incorrecta")
         return
     else:
-        hayQueGenerarArchivos = argv[0] 
+
+        # Parse arguments
+
+        generarArchivos = argv[0] 
         cantRecitales = int(argv[1]) #N
         cantBandas = int(argv[2]) #M
         maxBandasDistintasAContratar = int(argv[3]) #X
         maxRecitalesAParticiparPorBanda = int(argv[4]) #Y
 
-        if hayQueGenerarArchivos:
-            genArchivosRandom(cantRecitales, cantBandas)
+        # Initialize data:
+        #   - write/read files
+        #   - get rankings dictionary:
+        #       - [BAND_NUMBER][PREFERENCE ORDER] = RECITAL_NUMBER
+        #       - [RECITAL_NUMBER][PREFERENCE ORDER] = BAND_NUMBER
+
+        data = init(generarArchivos, cantRecitales, cantBandas, 
+            maxBandasDistintasAContratar, maxRecitalesAParticiparPorBanda)
+
+        bandasRanking = data[BANDA]
+        recitalesRanking = data[RECITAL]
+
+        # Perform matching
         
-        match(cantRecitales, cantBandas, 
+        match(bandasRanking, recitalesRanking, 
             maxBandasDistintasAContratar, maxRecitalesAParticiparPorBanda)
 
 main()
