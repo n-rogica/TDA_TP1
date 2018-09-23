509423324b5ebe17b1ce43c941e146686d0860fa
leandevesa-despegar <leandevesa@gmail.com>
2018-09-23 16:12:37 -0300

    data moved

--- codigo/Variante Gale Shapley/varianteGaleShapley.py
+++ codigo/Variante Gale Shapley/varianteGaleShapley.py
@@ -2,48 +2,54 @@ from sys import argv
 import random
 import os
 
-def genArchivosRandom(cantRecitales, cantBandas,
-maxBandasDistintasAContratar, maxRecitalesAParticiparPorBanda):
+DATA_PATH = "data/"
+BANDA = "banda"
+RECITAL = "recital"
+
+def genArchivoRandom(cant, nombre):
+    for num in range(1, cant + 1):
+        nombreArchivo = DATA_PATH + nombre + "_" + str(num) + ".dat"
+        archivo = open(nombreArchivo, "w")
+        preferencias = random.sample(range(1, cant + 1), cant)
+        for preferencia in preferencias:
+            archivo.write(str(preferencia) + os.linesep)
+        archivo.close()
+
+def genArchivosRandom(cantRecitales, cantBandas):
     print ("start gen archivos random")
 
-    #creo archivos de recitales
-    for numRecital in range(1,cantRecitales+1):
-        nombreArchivo = "recital_" + str(numRecital) + ".dat"
-        archivo = open(nombreArchivo, "w")
-        preferencias = random.sample(range(1,cantBandas+1),cantBandas)
-        for preferencia in preferencias:
-            archivo.write(str(preferencia) + os.linesep)
-        archivo.close()
-
-    #creo archivos de bandas
-    for numBanda in range(1,cantBandas+1):
-        nombreArchivo = "banda_" + str(numBanda) + ".dat"
-        archivo = open(nombreArchivo, "w")
-        preferencias = random.sample(range(1,cantRecitales+1),cantRecitales)
-        for preferencia in preferencias:
-            archivo.write(str(preferencia) + os.linesep)
-        archivo.close()
+    genArchivoRandom(cantRecitales, RECITAL)
+    genArchivoRandom(cantBandas, BANDA)
 
     print ("archivos random gen ok")
 
+def getDic(cant, nombre):
+
+    print("get dic ok")
+
+def match(cantRecitales, cantBandas, 
+        maxBandasDistintasAContratar, maxRecitalesAParticiparPorBanda):
+
+    print("match ok")
+
 def main():
 
-    argv = [10,10,10,10,10]
+    argv = [True,10,10,10,10] # TODO: Delete this test line
 
-    '''verifico que se ingresaron todos los parametros'''
     if(len(argv) < 5):
         print ("cantidad de parametros incorrecta")
         return
     else:
-        '''N'''
-        cantRecitales = int(argv[1])
-        '''M'''
-        cantBandas = int(argv[2])
-        '''X'''
-        maxBandasDistintasAContratar = int(argv[3])
-        '''Y'''
-        maxRecitalesAParticiparPorBanda = int(argv[4])
-        genArchivosRandom(cantRecitales, cantBandas,
+        hayQueGenerarArchivos = argv[0] 
+        cantRecitales = int(argv[1]) #N
+        cantBandas = int(argv[2]) #M
+        maxBandasDistintasAContratar = int(argv[3]) #X
+        maxRecitalesAParticiparPorBanda = int(argv[4]) #Y
+
+        if hayQueGenerarArchivos:
+            genArchivosRandom(cantRecitales, cantBandas)
+        
+        match(cantRecitales, cantBandas, 
             maxBandasDistintasAContratar, maxRecitalesAParticiparPorBanda)
 
 main()
